167c0a356e121fd7d07c9901937002c7f92eaf45
TanYS03 <117353315+TanYS03@users.noreply.github.com>
2023-05-28 02:24:49 +0800

    Add files via upload
    add mutual friends in UserFriends class and modify the User class

--- User.java
+++ User.java
@@ -1,21 +1,25 @@
-package application;
+package org.example;
 
+
+import jdk.internal.icu.text.UnicodeSet;
+
+import java.awt.desktop.UserSessionEvent;
 import java.io.Serializable;
 import java.util.*;
 import java.time.LocalDate;
-import java.time.Period;
 
-public class User implements Serializable {
+public class User implements Serializable, Comparable<User> {
     private String username;
     private String password;
     private String email;
     private String phoneNumber;
     private userInfo info;
     private userFriends friends;
-    private LocalDate birthday;
     private int id;
+    //private ArrayList<User> mutualFriends ;
+    private Admin admin;
 
-    public User(String username, String password, String email, String phoneNumber, LocalDate birthdayDate, int id) {
+    public User(String username, String password, String email, String phoneNumber, int id, boolean admin) {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -23,28 +27,20 @@ public class User implements Serializable {
         this.id = id;
         this.info = new userInfo();
         this.friends = new userFriends();
-        this.birthday = birthdayDate;
+        this.admin = new Admin(admin);
+
+        //this.mutualFriends = friends.getMutualFriends();
     }
 
-    // Getters and setters 
+    public User(){}
+
+    // Getters and setters for username, password, email, and phoneNumber
     public String getUsername() {
         return username;
     }
-    
+
     public int getId() {
-    	return id;
-    }
-    
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-    
-    public int getAge() {
-    	if (birthday != null) {
-        return Period.between(birthday, LocalDate.now()).getYears();
-    	} else {
-			return 0;
-		}
+        return id;
     }
 
     public void setUsername(String username) {
@@ -74,176 +70,84 @@ public class User implements Serializable {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-    
-    public void displayInfo() {
-    	System.out.println("Username: " + this.username);
-    	System.out.println("Name: " + info.getName());
-    	System.out.println("Id: " + this.id);
-    	System.out.println("Birthday: " + this.getBirthday());
-    	System.out.println("Age: " + this.getAge());
-    	System.out.println("Gender: " + info.getGender());
-    	System.out.println("Job(s): " + info.printArraylist(info.getJobs()));
-    	System.out.println("Hobbies: " + info.printArraylist(info.getHobbies()));
-    }
-    
-    public void displayInfo2(User currentUser) {
-    	System.out.println("Username: " + this.username);
-    	System.out.println("Name: " + info.getName());
-    	System.out.println("Id: " + this.id);
-    	System.out.println("Birthday: " + this.getBirthday());
-    	System.out.println("Age: " + this.getAge());
-    	System.out.println("Gender: " + info.getGender());
-    	System.out.println("Job(s): " + info.printArraylist(info.getJobs()));
-    	System.out.println("Hobbies: " + info.printArraylist(info.getHobbies()));
-    	System.out.println("Mutual friend(s): " + friends.mutualFriends(currentUser).size());
-    	System.out.print("List of Mutual Friends: ");
-    	for (User user: friends.mutualFriends(currentUser)) {
-			System.out.print(user.getUsername() + " ");
-		}
-    	System.out.println("");
-    	
-    }
-    
-    public void setInfo() {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("Enter to skip");
-    	
-    	System.out.print("Name: ");
-    	String name = sc.nextLine();
-    	if (!name.isBlank()) {
-    		info.setName(name);
-    	}
-    	
-    	System.out.print("Gender: ");
-    	String gender = sc.nextLine();
-    	if (!gender.isBlank()) {
-    		info.setGender(gender);
-    	}
-    	
-    	System.out.print("Address: ");
-    	String address = sc.nextLine();
-    	if (!address.isBlank()) {
-    		info.setAddress(address);
-    	}
-    	
-    	System.out.print("School: ");
-    	String School = sc.nextLine();
-    	if (!School.isBlank()) {
-    		info.setSchool(School);
-    	}
-    	
-    	System.out.print("Relationship status: ");
-    	String status = sc.nextLine();
-    	if (!status.isBlank()) {
-    		info.setRelationshipStatus(status);
-    	}
-    	
-    	System.out.print("Jobs (separate with ,): ");
-    	String jobs = sc.nextLine();
-    	if (!jobs.isBlank()) {
-    		String[] job = jobs.split(",");
-    		info.setJobs(job);
-    	}
-    	
-    	System.out.print("Hobbies (separate with ,): ");
-    	String hobbies = sc.nextLine();
-    	if (!hobbies.isBlank()) {
-    		String[] hob = hobbies.split(",");
-    		info.setHobbies(hob);
-    	}
-    	
-    }
-    
-    public void updateFriend() {
-    	int numofFriend = friends.getFriends().size();
-    	info.updateFriends(numofFriend);
-    }
-    
-    public boolean isFriend(User user) {
-    	return friends.checkifFriend(user);
-    }
-    
-    public void addFriend(User user) {
-    	if (!isFriend(user)) {
-    	friends.addFriends(user);
-    	user.friends.addFriends(this);
-    	System.out.printf("Successfully sent a friend request to %s\n", user.getUsername());
-    	} else {
-			System.out.println("Already a friend");
-		}
-    }
-    
-    public void removeFriend(User user) {
-    	if (isFriend(user)) {
-    		removeFriend(user);
-    		user.removeFriend(this);
-    		updateFriend();
-    		user.updateFriend();
-    		System.out.printf("%s successfully removed as a friend\n", user.getUsername());
-    	} else {
-    		System.out.printf("%s is not your friend\n", user.getUsername());
-		}
-    }
-    
-    public void seePending() {
-    	Scanner scanner = new Scanner(System.in);
-    	for (User user: friends.getPending()) {
-    		System.out.println(user.getUsername());
-    	}
-    	System.out.println("Which user do you want to interact with?");
-    	int toInteract = scanner.nextInt() - 1;
-    	if (toInteract > friends.getPending().size()) {
-    		System.out.println("Invalid index");
-    	} else {
-			System.out.println("1 to accept, 2 to decline");
-			int action = scanner.nextInt();
-			switch (action) {
-			case 1: 
-				acceptFriend(friends.getPending().get(toInteract));
-				break;
-			case 2:
-				declineFriend(friends.getPending().get(toInteract));
-				break;
-			default:
-				System.out.println("type 1 or 2 only");
-			}
-		}
-    	
-    }
-    
-    
-    public void acceptFriend(User user) {
-    	friends.acceptFriend(user);
-    	user.friends.acceptFriend(this);
-    	updateFriend();
-    	user.updateFriend();
-    }
-    
-    public void declineFriend(User user) {
-    	friends.declineFriend(user);
-    	user.friends.declineFriend(this);
-    }
-    
+
+
+//    public ArrayList<User> getMutualFriends() {
+//        return mutualFriends;
+//    }
+
     public userFriends getFriends() {
-		return friends;
-	}
-    
-    public void getFriendSuggestion(ArrayList<User> UsersList) {
-    	System.out.println("Lists of suggested friends: ");
-    	int amount = 0;
-    	for(User user : friends.suggestedFriends(UsersList, this)) {
-    		System.out.println(user.getUsername() + ": " + friends.mutualFriends(user).size());
-    		if (amount == 5) {
-				break;
-			}
-    		amount++;
-    	}
-    	if (amount == 0) {
-			System.out.println("No suggested friends found");
-		}
+        return friends;
     }
+
+    public void displayInfo() {
+        System.out.println("Username: " + this.username);
+        System.out.println("Name: " + info.getName());
+        System.out.println("Id: " + this.id);
+        System.out.println("Birthday: " + info.getBirthday());
+        System.out.println("Age: " + info.getAge());
+        System.out.println("Gender: " + info.getGender());
+        System.out.println("Job(s): " + info.getJobs());
+        System.out.println("Hobbies: " + info.printArraylist(info.getHobbies()));
+    }
+
+    public void setInfo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter to skip");
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        if (!name.isBlank()) {
+            info.setName(name);
+        }
+
+        System.out.print("Birthday (year): ");
+        String year = sc.nextLine();
+        if (!year.isBlank()) {
+            System.out.print("Birthday (month): ");
+            String month = sc.nextLine();
+            if (!month.isBlank()) {
+                System.out.print("Birthday (date): ");
+                String date = sc.nextLine();
+                if (!date.isBlank()) {
+                    LocalDate birthdayDate = LocalDate.of(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(date));
+                    info.setBirthday(birthdayDate);
+                }
+            }
+        }
+
+        System.out.print("Gender: ");
+        String gender = sc.nextLine();
+        if (!gender.isBlank()) {
+            info.setGender(gender);
+        }
+
+        System.out.print("Job: ");
+        String Job = sc.nextLine();
+        if (!Job.isBlank()) {
+            info.setJobs(Job);
+        }
+
+        System.out.print("Hobbies (separate with ,): ");
+        String hobbies = sc.nextLine();
+        if (!hobbies.isBlank()) {
+            String[] hob = hobbies.split(",");
+            info.setHobbies(hob);
+        }
+
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin.setAdmins(admin);
+    }
+
+    @Override
+    public int compareTo(User user){
+        if(this.getId() > user.getId()) return 1;
+        else if (this.getId() == user.getId()) { return 0;
+        }
+        else return -1;
+    }
+
+
 }
--- userFriends.java
+++ userFriends.java
@@ -1,118 +1,86 @@
-package application;
+package org.example;
+
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
 
 
 //empty for now haven't done anything in it
-public class userFriends implements Serializable {
-	private ArrayList<User> friends;
-	private ArrayList<User> pending;
-    
-    
-	public userFriends() {
-		friends = new ArrayList<User>();
-		pending = new ArrayList<User>();
-		
-	}
-	
-	public ArrayList<User> getFriends(){
-		return friends;
-	}
-	
-	public ArrayList<User> getPending(){
-		return pending;
-	}
-	
-	public boolean checkifFriend(User user) {
-		if (friends.contains(user)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean checkifPending(User user) {
-		if (pending.contains(user)) {
-			return true;
-		}
-		return false;
-	}	
-	
-	public void addFriends(User user) {
-		pending.add(user);
-	}
-	
-	public void acceptFriend(User user) {
-		if	(checkifPending(user)) {
-			pending.remove(user);
-			friends.add(user);
-		}
-	}
-	
-	public void declineFriend(User user) {
-		if	(checkifPending(user)) {
-			pending.remove(user);
-		}
-	}
-	
-	public void removeFriends(User user) {
-		for (User account : friends) {
-			if (account.equals(user)) {
-				friends.remove(account);
-			}
-		}
-	}
-	
-	public ArrayList<User> mutualFriends(User user) {
-		ArrayList<User> MutualFriends = new ArrayList<User>();
-		int mutuals = 0;
-		for (User friends : user.getFriends().getFriends()) {
-			for (User friends2 : this.getFriends()) {
-				if (friends.equals(friends2)) {
-					MutualFriends.add(friends);
-					mutuals++;
-					break;
-				}
-			}
-		}
-		return MutualFriends;
-	}
-	
-	public ArrayList<User> suggestedFriends(ArrayList<User> listOfUsers, User cUser){
-		ArrayList<User> suggestedFriends = new ArrayList<User>();
-		for(User users : listOfUsers) {
-			if (checkIfEligible(users) && !cUser.equals(users)) {
-				if (mutualFriends(users).size() != 0) {
-				suggestedFriends.add(users);
-				}
-			}
-		}
-		
-		sortUsersByMutualFriends(suggestedFriends);
-		return suggestedFriends;
-	}
-	
-	private boolean checkIfEligible(User user){
-		if (checkifFriend(user) || checkifPending(user)) {
-			return false;
-		}
-		return true;
-	}
-	
-	private ArrayList<User> sortUsersByMutualFriends(ArrayList<User> listOfUsers) {
-		for (int i = 0; i < listOfUsers.size() - 1; i++) {
-            for (int j = i + 1; j < listOfUsers.size(); j++) {
-                User user1 = listOfUsers.get(i);
-                User user2 = listOfUsers.get(j);
-                if (this.mutualFriends(user1).size() < this.mutualFriends(user2).size()) {
-                    // Swap the users
-                    listOfUsers.set(i, user2);
-                    listOfUsers.set(j, user1);
+public class userFriends implements Serializable{
+    private ArrayList<User> friends;
+    private ArrayList<User> pending;
+    private ArrayList<User> blocked;
+
+    private ArrayList<User> mutualFriends;
+
+    public userFriends() {
+        friends = new ArrayList<User>();
+        pending = new ArrayList<User>();
+        blocked = new ArrayList<User>();
+        mutualFriends = new ArrayList<>();
+    }
+
+    public ArrayList<User> getFriends(){
+        return friends;
+    }
+
+    public ArrayList<User> getPending(){
+        return pending;
+    }
+
+    public ArrayList<User> getMutualFriends() {
+        return mutualFriends;
+    }
+
+    public boolean checkifFriend(User user) {
+        if (friends.contains(user)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkifPending(User user) {
+        if (pending.contains(user)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public void addFriends(User user) {
+        pending.add(user);
+    }
+
+    public void acceptFriend(User user) {
+        if	(checkifPending(user)) {
+            pending.remove(user);
+            friends.add(user);
+        }
+    }
+
+    public void declineFriend(User user) {
+        if	(checkifPending(user)) {
+            pending.remove(user);
+        }
+    }
+
+    public void removeFriends(User user) {
+        for (User account : friends) {
+            if (account.equals(user)) {
+                friends.remove(account);
+            }
+        }
+    }
+
+    public void setMutualFriends(User user){
+        for (int i = 0; i < friends.size(); i++) { //my friend
+            userFriends friend = user.getFriends();
+            for (int j = 0; j < friend.friends.size(); j++) {// another user friends
+                if(friends.get(i).equals(friend.friends.get(j))){
+                    mutualFriends.add(friends.get(i));
                 }
             }
         }
-		return listOfUsers;
-	}
-	
+    }
 }
