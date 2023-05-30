badce11de0d78ec23076918bee9569bff9455f75
TanYS03 <117353315+TanYS03@users.noreply.github.com>
2023-05-30 20:31:33 +0800

    Add files via upload
    Add password hashing features and modify the verify password to password hash in login method

--- LoginSystem.java
+++ LoginSystem.java
@@ -1,6 +1,8 @@
-package application;
+package org.example;
 
 import java.io.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDate;
@@ -28,44 +30,44 @@ public class LoginSystem {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a username: ");
         String username = scanner.nextLine();
-        
+
         System.out.print("Enter email: ");
         String email = scanner.nextLine();
         System.out.print("Enter phone number: ");
         String phone = scanner.nextLine();
-        
-        
+
+
         System.out.print("Enter birthday(Date/Month/Year eg.21/12/2021): ");
         String birthday = scanner.nextLine();
-        String[] birthDayArray = birthday.split("/"); 
-        
+        String[] birthDayArray = birthday.split("/");
+
         LocalDate birthdayDate = LocalDate.of(Integer.valueOf(birthDayArray[2]), Integer.valueOf(birthDayArray[1]), Integer.valueOf(birthDayArray[0]));
         boolean flag = true;
-        
+
         if ( Period.between(birthdayDate, LocalDate.now()).getYears() < 13) {
             flag = false;
             System.out.println("You must be at least 13 years old to use this service");
         }
-        
+
         if (flag) {
-        System.out.print("Enter a password: ");
-        String password = scanner.nextLine();
-        
-        System.out.print("Retype the password: ");
-        String Repassword = scanner.nextLine();
-        
-        if(password.equals(Repassword) && flag) {
-        login_info.add(new User(username, password, email, phone, birthdayDate, login_info.size())); // add the account to the login_info arraylist
-        System.out.println("Account created successfully.");
-        saveToFile();
-        } else {
-			System.out.println("Password does not match");
-		}
-        }    
+            System.out.print("Enter a password: ");
+            String password = scanner.nextLine();
+
+            System.out.print("Retype the password: ");
+            String Repassword = scanner.nextLine();
+
+            if(password.equals(Repassword) && flag) {
+                login_info.add(new User(username, password, email, phone, birthdayDate, login_info.size())); // add the account to the login_info arraylist
+                System.out.println("Account created successfully.");
+                saveToFile();
+            } else {
+                System.out.println("Password does not match");
+            }
+        }
     }
 
     // Method to check if a username and password are valid and return true if they are
-    public boolean login() {
+    public boolean login() throws NoSuchAlgorithmException, InvalidKeySpecException {
         if (loggedIn) {
             System.out.println("You are already logged in. Please log out before logging in again.");
             return false;
@@ -76,25 +78,25 @@ public class LoginSystem {
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
         for (User account : login_info) {
-            if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
+            if (account.getUsername().equals(username) && account.getHash().equals(currentUser.passwordHash(password))) { /** change to password hashing */
                 System.out.println("Login successful.");
                 loggedIn = true;
                 currentUser = account;
                 currentUserId = account.getId();
                 return true;
-            } else if (account.getEmail().equals(username) && account.getPassword().equals(password)) {
+            } else if (account.getEmail().equals(username) && account.getHash().equals(currentUser.passwordHash(password))) {
                 System.out.println("Login successful.");
                 loggedIn = true;
                 currentUser = account;
                 currentUserId = account.getId();
                 return true;
-            } else if (account.getPhoneNumber().equals(username) && account.getPassword().equals(password)) {
+            } else if (account.getPhoneNumber().equals(username) && account.getHash().equals(currentUser.passwordHash(password))) {
                 System.out.println("Login successful.");
                 loggedIn = true;
                 currentUser = account;
                 currentUserId = account.getId();
                 return true;
-            } 
+            }
         }
         System.out.println("Invalid username or password.");
         return false;
@@ -115,7 +117,7 @@ public class LoginSystem {
     // Method to save login_info to a file
     public void saveToFile() {
         try {
-        	String fileName = "login_info";
+            String fileName = "login_info";
             FileOutputStream fileOut = new FileOutputStream(fileName);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(login_info);
@@ -129,7 +131,7 @@ public class LoginSystem {
 
     public void loadFromFile() {
         try {
-        	String fileName = "login_info";
+            String fileName = "login_info";
             FileInputStream fileIn = new FileInputStream(fileName);
             ObjectInputStream in = new ObjectInputStream(fileIn);
             login_info = (ArrayList<User>) in.readObject();
@@ -144,43 +146,46 @@ public class LoginSystem {
     }
     //setter getter
     public ArrayList<User> getUsers(){
-    	return login_info;
+        return login_info;
     }
-    
+
     public User getUser(int id){
-    	return login_info.get(id);
-    	
+        return login_info.get(id);
+
     }
-    
+
     public int getCurrentId() {
-    	return currentUserId;
+        return currentUserId;
     }
-    
+
     public boolean isLoggedIn() {
-    	if(loggedIn) {
-    		return true;
-    	}
-    	System.out.println("You need to be logged in");
-    	return false;
+        if(loggedIn) {
+            return true;
+        }
+        System.out.println("You need to be logged in");
+        return false;
     }
-    
+
     public User getCurrentUser() {
-    	return currentUser;
+        return currentUser;
     }
-    
+
     public void displayPendingFriends() {
-    	currentUser.seePending();
+        currentUser.seePending();
     }
-    
+
     public void displayAllUsers() {
-    	for (User user : login_info) {
-			System.out.println(user.getUsername());
-		}
+        for (User user : login_info) {
+            System.out.println(user.getUsername());
+        }
     }
-    
+
     public void displaySuggestedFriends() {
-    	currentUser.getFriendSuggestion(login_info);
+        currentUser.getFriendSuggestion(login_info);
+    }
+
+    public ArrayList<User> getLogin_info() {
+        return login_info;
     }
 }
-	
-	
+
--- User.java
+++ User.java
@@ -1,25 +1,35 @@
 package org.example;
 
 
-import jdk.internal.icu.text.UnicodeSet;
 
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.awt.desktop.UserSessionEvent;
 import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.*;
 import java.time.LocalDate;
+import java.time.Period;
 
-public class User implements Serializable, Comparable<User> {
+public class User implements Serializable {
     private String username;
     private String password;
+
     private String email;
     private String phoneNumber;
     private userInfo info;
     private userFriends friends;
+    private LocalDate birthday;
     private int id;
-    //private ArrayList<User> mutualFriends ;
     private Admin admin;
+    private byte[] salt;
+    private byte[] hash;
 
-    public User(String username, String password, String email, String phoneNumber, int id, boolean admin) {
+    public User(String username, String password, String email, String phoneNumber, LocalDate birthdayDate, int id)  {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -27,9 +37,16 @@ public class User implements Serializable, Comparable<User> {
         this.id = id;
         this.info = new userInfo();
         this.friends = new userFriends();
-        this.admin = new Admin(admin);
-
-        //this.mutualFriends = friends.getMutualFriends();
+        this.birthday = birthdayDate;
+        this.admin = new Admin(false);
+        try {
+            this.hash = passwordHash(password);
+        }catch (NoSuchAlgorithmException e) {
+            System.out.println("No such algorithm");
+        }
+        catch(InvalidKeySpecException e){
+            System.out.println("Th Key Spec is invalid");
+        }
     }
 
     public User(){}
@@ -45,6 +62,19 @@ public class User implements Serializable, Comparable<User> {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge(){
+        if(birthday != null){
+            return Period.between(birthday,LocalDate.now()).getYears();
+        }
+        else{
+            return 0;
+        }
     }
 
     public String getPassword() {
@@ -71,24 +101,45 @@ public class User implements Serializable, Comparable<User> {
         this.phoneNumber = phoneNumber;
     }
 
-
-//    public ArrayList<User> getMutualFriends() {
-//        return mutualFriends;
-//    }
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 
     public userFriends getFriends() {
         return friends;
+    }
+
+    public byte[] getHash() {
+        return hash;
     }
 
     public void displayInfo() {
         System.out.println("Username: " + this.username);
         System.out.println("Name: " + info.getName());
         System.out.println("Id: " + this.id);
-        System.out.println("Birthday: " + info.getBirthday());
-        System.out.println("Age: " + info.getAge());
+        System.out.println("Birthday: " + this.getBirthday());
+        System.out.println("Age: " + this.getAge());
         System.out.println("Gender: " + info.getGender());
-        System.out.println("Job(s): " + info.getJobs());
+        System.out.println("Job(s): " + info.printArraylist(info.getJobs()));
         System.out.println("Hobbies: " + info.printArraylist(info.getHobbies()));
+    }
+
+    public void displayInfo2(User currentUser) {
+        System.out.println("Username: " + this.username);
+        System.out.println("Name: " + info.getName());
+        System.out.println("Id: " + this.id);
+        System.out.println("Birthday: " + this.getBirthday());
+        System.out.println("Age: " + this.getAge());
+        System.out.println("Gender: " + info.getGender());
+        System.out.println("Job(s): " + info.printArraylist(info.getJobs()));
+        System.out.println("Hobbies: " + info.printArraylist(info.getHobbies()));
+        System.out.println("Mutual friend(s): " + friends.mutualFriends(currentUser).size());
+        System.out.print("List of Mutual Friends: ");
+        for (User user: friends.mutualFriends(currentUser)) {
+            System.out.print(user.getUsername() + " ");
+        }
+        System.out.println("");
+
     }
 
     public void setInfo() {
@@ -101,31 +152,35 @@ public class User implements Serializable, Comparable<User> {
             info.setName(name);
         }
 
-        System.out.print("Birthday (year): ");
-        String year = sc.nextLine();
-        if (!year.isBlank()) {
-            System.out.print("Birthday (month): ");
-            String month = sc.nextLine();
-            if (!month.isBlank()) {
-                System.out.print("Birthday (date): ");
-                String date = sc.nextLine();
-                if (!date.isBlank()) {
-                    LocalDate birthdayDate = LocalDate.of(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(date));
-                    info.setBirthday(birthdayDate);
-                }
-            }
-        }
-
         System.out.print("Gender: ");
         String gender = sc.nextLine();
         if (!gender.isBlank()) {
             info.setGender(gender);
         }
 
-        System.out.print("Job: ");
-        String Job = sc.nextLine();
-        if (!Job.isBlank()) {
-            info.setJobs(Job);
+        System.out.print("Address: ");
+        String address = sc.nextLine();
+        if (!address.isBlank()) {
+            info.setAddress(address);
+        }
+
+        System.out.print("School: ");
+        String School = sc.nextLine();
+        if (!School.isBlank()) {
+            info.setSchool(School);
+        }
+
+        System.out.print("Relationship status: ");
+        String status = sc.nextLine();
+        if (!status.isBlank()) {
+            info.setRelationshipStatus(status);
+        }
+
+        System.out.print("Jobs (separate with ,): ");
+        String jobs = sc.nextLine();
+        if (!jobs.isBlank()) {
+            String[] job = jobs.split(",");
+            info.setJobs(job);
         }
 
         System.out.print("Hobbies (separate with ,): ");
@@ -135,13 +190,98 @@ public class User implements Serializable, Comparable<User> {
             info.setHobbies(hob);
         }
 
+
+    }
+
+    public void updateFriend() {
+        int numofFriend = friends.getFriends().size();
+        info.updateFriends(numofFriend);
+    }
+
+    public boolean isFriend(User user) {
+        return friends.checkifFriend(user);
+    }
+
+    public void addFriend(User user) {
+        if (!isFriend(user)) {
+            friends.addFriends(user);
+            user.friends.addFriends(this);
+            System.out.printf("Successfully sent a friend request to %s\n", user.getUsername());
+        } else {
+            System.out.println("Already a friend");
+        }
     }
 
     public void setAdmin(boolean admin) {
         this.admin.setAdmins(admin);
     }
 
-    @Override
+    public void removeFriend(User user) {
+        if (isFriend(user)) {
+            removeFriend(user);
+            user.removeFriend(this);
+            updateFriend();
+            user.updateFriend();
+            System.out.printf("%s successfully removed as a friend\n", user.getUsername());
+        } else {
+            System.out.printf("%s is not your friend\n", user.getUsername());
+        }
+    }
+
+    public void seePending() {
+        Scanner scanner = new Scanner(System.in);
+        for (User user: friends.getPending()) {
+            System.out.println(user.getUsername());
+        }
+        System.out.println("Which user do you want to interact with?");
+        int toInteract = scanner.nextInt() - 1;
+        if (toInteract > friends.getPending().size()) {
+            System.out.println("Invalid index");
+        } else {
+            System.out.println("1 to accept, 2 to decline");
+            int action = scanner.nextInt();
+            switch (action) {
+                case 1:
+                    acceptFriend(friends.getPending().get(toInteract));
+                    break;
+                case 2:
+                    declineFriend(friends.getPending().get(toInteract));
+                    break;
+                default:
+                    System.out.println("type 1 or 2 only");
+            }
+        }
+
+    }
+
+
+    public void acceptFriend(User user) {
+        friends.acceptFriend(user);
+        user.friends.acceptFriend(this);
+        updateFriend();
+        user.updateFriend();
+    }
+
+    public void declineFriend(User user) {
+        friends.declineFriend(user);
+        user.friends.declineFriend(this);
+    }
+
+    public void getFriendSuggestion(ArrayList<User> UsersList) {
+        System.out.println("Lists of suggested friends: ");
+        int amount = 0;
+        for(User user : friends.suggestedFriends(UsersList, this)) {
+            System.out.println(user.getUsername() + ": " + friends.mutualFriends(user).size());
+            if (amount == 5) {
+                break;
+            }
+            amount++;
+        }
+        if (amount == 0) {
+            System.out.println("No suggested friends found");
+        }
+    }
+
     public int compareTo(User user){
         if(this.getId() > user.getId()) return 1;
         else if (this.getId() == user.getId()) { return 0;
@@ -149,5 +289,14 @@ public class User implements Serializable, Comparable<User> {
         else return -1;
     }
 
+    public byte[] passwordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecureRandom random = new SecureRandom();
+        salt = new byte[16];
+        random.nextBytes(salt);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        hash = factory.generateSecret(spec).getEncoded();
+        return hash;
+    }
 
 }
