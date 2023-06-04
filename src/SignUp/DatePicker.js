c7c4616bc4f23d5d2f315183ae835c78e21091a4
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2023-06-05 01:20:23 +0800

    Update date picker for dob and gender in sign up part

--- src/SignUp/DatePicker.js
+++ src/SignUp/DatePicker.js
@@ -1,49 +1,10 @@
-// import { DatePicker, Space } from 'antd';
-// import React from 'react';
-
-// const { RangePicker } = DatePicker;
-
-// const DateSelector = () => (
-//   <Space direction="vertical" size={12}>
-//     <DatePicker
-//       cellRender={(current) => {
-//         const style = {};
-//         if (current.date() === 1) {
-//           style.border = '1px solid #1677ff';
-//           style.borderRadius = '50%';
-//           style.height = '100px';
-//         }
-//         return (
-//           <div className="ant-picker-cell-inner" style={style}>
-//             {current.date()}
-//           </div>
-//         );
-//       }}
-//     />
-//   </Space>
-// );
-
-// export default DateSelector;
-
 import { DatePicker, Space } from 'antd';
-
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
 const App = () => (
-  <Space direction="vertical" size={12}>
-    <DatePicker
-      cellRender={(current) => {
-        const style = {};
-        if (current.date() === 1) {
-          style.border = '1px solid #1677ff';
-          style.borderRadius = '50%';
-        }
-        return (
-          <div className="ant-picker-cell-inner" style={style}>
-            {current.date()}
-          </div>
-        );
-      }}
-    />
+  <Space direction="vertical">
+    <DatePicker onChange={onChange} />   
   </Space>
 );
-
 export default App;
